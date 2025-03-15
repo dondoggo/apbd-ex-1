d8e63e1da35a07f36f9159a7b40d9006bcc9c098
dondoggo <s27004@pjwstk.edu.pl>
2025-03-15 19:00:19 +0100

    Modyfikacja 3

--- Program/Program.cs
+++ Program/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Witaj w programie!");
+        Console.Write("Jak masz na imię? ");
+        string imie = Console.ReadLine();
+
+        Console.WriteLine($"Witaj, {imie}!");
+
+        Console.Write("Ile masz lat? ");
+        int wiek = int.Parse(Console.ReadLine()); 
+        int lataDo100 = 100 - wiek;
+        Console.WriteLine($"Za {lataDo100} lat będziesz miał(a) 100 lat.");
     }
 }
