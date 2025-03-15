71cba57e8962c0310451838fe20583ec34d79a68
dondoggo <s27004@pjwstk.edu.pl>
2025-03-15 18:59:28 +0100

    Modyfikacja 2

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello APBD");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
